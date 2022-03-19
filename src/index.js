4902a2e2bbf826816cb1ee9b5603f6c03b7ea9fd
kunida <67056950+kunida@users.noreply.github.com>
2022-03-19 11:00:56 +0900

    タスク（テキスト）の追加

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //テキストボックスの値を取得し初期化
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //liタグ生成
+  const li = document.createElement("li");
+
+  //divタグ生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //pタグ生成、入力した内容追加
+  const todo = document.createElement("p");
+  todo.innerText = inputText;
+
+  //各要素を設定
+  li.appendChild(div);
+  div.appendChild(todo);
+
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
